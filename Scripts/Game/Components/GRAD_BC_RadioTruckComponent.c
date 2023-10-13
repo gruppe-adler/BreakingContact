a61a8a5b7b850d31450b60c2dad7004740aee3d5
y0014984 <y0014984@gmail.com>
2023-10-13 09:54:25 +0200

    added radio transmission user action

--- Scripts/Game/Components/GRAD_BC_RadioTruckComponent.c
+++ Scripts/Game/Components/GRAD_BC_RadioTruckComponent.c
@@ -8,10 +8,15 @@ class GRAD_BC_RadioTruckComponent : ScriptComponent
 	[Attribute(defvalue: "2000", uiwidget: UIWidgets.EditBox, desc: "Update Interval for checking EngineOn() and show/hide marker", params: "", category: "Breaking Contact - Radio Truck")];
 	protected int m_iMarkerUpdateInterval;
 	
-	protected Vehicle m_radioTruck;
+	//[RplProp()]
+	protected ERadioTransmissionState m_eRadioTransmissionState;
 	
-	protected SCR_MapDescriptorComponent m_mapDescriptorComponent;
-	protected VehicleWheeledSimulation_SA m_VehicleWheeledSimulationComponent;
+	private Vehicle m_radioTruck;
+	
+	private SCR_MapDescriptorComponent m_mapDescriptorComponent;
+	private VehicleWheeledSimulation_SA m_VehicleWheeledSimulationComponent;
+	
+	private RplComponent m_RplComponent;
 	
 	//------------------------------------------------------------------------------------------------
 	override void OnPostInit(IEntity owner)
@@ -23,13 +28,27 @@ class GRAD_BC_RadioTruckComponent : ScriptComponent
 		m_mapDescriptorComponent = SCR_MapDescriptorComponent.Cast(m_radioTruck.FindComponent(SCR_MapDescriptorComponent));
 		m_VehicleWheeledSimulationComponent = VehicleWheeledSimulation_SA.Cast(m_radioTruck.FindComponent(VehicleWheeledSimulation_SA));
 		
-		GetGame().GetCallqueue().CallLater(SetRadioTruckMarkerVisibility, m_iMarkerUpdateInterval, true); 
+		m_RplComponent = RplComponent.Cast(m_radioTruck.FindComponent(RplComponent));
+		
+		PrintFormat("BC Debug - IsMaster(): %1", m_RplComponent.IsMaster()); // IsMaster() does not mean Authority
+		PrintFormat("BC Debug - IsProxy(): %1", m_RplComponent.IsProxy());
+		PrintFormat("BC Debug - IsOwner(): %1", m_RplComponent.IsOwner());
+		
+		//GetGame().GetCallqueue().CallLater(SetRadioTruckMarkerVisibility, m_iMarkerUpdateInterval, true); 
+		
+		// Initially set radio transmission state to off and disable the map marker
+		//SetRadioTransmissionState(ERadioTransmissionState.OFF);
+		GetGame().GetCallqueue().CallLater(SetRadioTransmissionState, 5000, false, ERadioTransmissionState.OFF); 
 	}
 	
+	
 	//------------------------------------------------------------------------------------------------
-	void SetRadioTruckMarkerVisibility()
+	protected void SetRadioTruckMarkerVisibility(bool enableMarkerVisibility)
 	{
+		// this function seems to run server-side only
+		
 		//Print("BC Debug - CheckMarker()", LogLevel.NORMAL);
+		
 		
 		if (!m_radioTruck)
 		{
@@ -49,17 +68,72 @@ class GRAD_BC_RadioTruckComponent : ScriptComponent
 			return;
 		}
 	
-		if (m_VehicleWheeledSimulationComponent.EngineIsOn())
-		{			
-			m_mapDescriptorComponent.Item().SetVisible(false);
-			//SCR_HintManagerComponent.GetInstance().ShowCustomHint("MARKER OFF");
-			//Print("BC Debug - marker off", LogLevel.NORMAL);
+		// m_VehicleWheeledSimulationComponent.EngineIsOn()
+		
+		if (enableMarkerVisibility)
+		{
+			m_mapDescriptorComponent.Item().SetVisible(true);
+			//Rpc(RpcAsk_Authority_SetMarkerVisibility, true);
+			//SCR_HintManagerComponent.GetInstance().ShowCustomHint("MARKER ON");
+			Print("BC Debug - marker on", LogLevel.NORMAL);
 		}
 		else
 		{
-			m_mapDescriptorComponent.Item().SetVisible(true);
-			//SCR_HintManagerComponent.GetInstance().ShowCustomHint("MARKER ON");
-			//Print("BC Debug - marker on", LogLevel.NORMAL);
+			m_mapDescriptorComponent.Item().SetVisible(false);
+			//Rpc(RpcAsk_Authority_SetMarkerVisibility, false);
+			//SCR_HintManagerComponent.GetInstance().ShowCustomHint("MARKER OFF");
+			Print("BC Debug - marker off", LogLevel.NORMAL);
 		}
 	}
+	
+	//------------------------------------------------------------------------------------------------
+	ERadioTransmissionState GetRadioTransmissionState()
+	{
+		return m_eRadioTransmissionState;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void SetRadioTransmissionState(ERadioTransmissionState radioTransmissionState)
+	{
+		m_eRadioTransmissionState = radioTransmissionState;
+		
+		if (m_eRadioTransmissionState == ERadioTransmissionState.TRANSMITTING)
+			SetRadioTruckMarkerVisibility(true);
+		else
+			SetRadioTruckMarkerVisibility(false);
+	}
+	
+	/*
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
+	protected void RpcAsk_Authority_SetMarkerVisibility(bool isVisible)
+	{
+		Print("BC Debug - RpcAsk_Authority_SetMarkerVisibility()", LogLevel.NORMAL);
+		
+		m_mapDescriptorComponent.Item().SetVisible(isVisible);
+		
+		m_bIsVisible = isVisible;
+
+		Replication.BumpMe();
+		
+		Rpc(RpcDo_Broadcast_SetMarkerVisibility, isVisible);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Broadcast)]
+	protected void RpcDo_Broadcast_SetMarkerVisibility(bool isVisible)
+	{
+		Print("BC Debug - RpcDo_Broadcast_SetMarkerVisibility()", LogLevel.NORMAL);
+		
+		m_mapDescriptorComponent.Item().SetVisible(isVisible);
+	}
+	*/
+}
+
+enum ERadioTransmissionState
+{
+	OFF,
+	TRANSMITTING,
+	INTERRUPTED,
+	DONE
 }

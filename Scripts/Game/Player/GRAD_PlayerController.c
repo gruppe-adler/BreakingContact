18dee1b32a3e525c0217887905cc31462f251d42
nomisum <nomisum@gmail.com>
2024-01-31 23:47:42 +0100

    state of affairs - wonky but workable

--- Scripts/Game/Player/GRAD_PlayerController.c
+++ Scripts/Game/Player/GRAD_PlayerController.c
@@ -10,8 +10,8 @@ modded class SCR_PlayerController : PlayerController
 	override void EOnInit(IEntity owner)
 	{
 		super.EOnInit(owner);
-		InitMapMarkerUI();
-		GetGame().GetCallqueue().CallLater(ForceOpenMap, 5000, false);
+		GetGame().GetCallqueue().CallLater(InitMapMarkerUI, 30000, false);
+		GetGame().GetCallqueue().CallLater(ForceOpenMap, 30000, false);
 		m_bChoosingSpawn = true;
     }
 			
